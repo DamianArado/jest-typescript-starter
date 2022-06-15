ea7c71ebddc5093355cd2659cc31038735e8c4e4
DamianArado <khanhumayun95@gmail.com>
2022-06-15 21:55:12 +0530

    Add quick starter

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    roots: ['<rootDir>/src/test'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    },
+    testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    verbose: true,
+    collectCoverage: true,
+    collectCoverageFrom: ['<rootDir>/src/app/**/*.ts']
+}
---
+++ src/test/demo.test.ts
@@ -0,0 +1,6 @@
+describe('Your first test suite', () => {
+	test("Your first test case", () => {
+		console.log("Hell yeah!");
+		expect(true).toBe(true);
+	});
+});
